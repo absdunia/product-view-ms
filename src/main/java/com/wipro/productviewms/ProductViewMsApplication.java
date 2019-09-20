90bde32767076224cc0ec7d3cbb8bc8737aea26d
Abhishek Roy <roy.of.abhishek13@gmail.com>
2019-09-20 13:05:17 +0530

    product view

---
+++ src/main/java/com/wipro/productviewms/ProductViewMsApplication.java
@@ -0,0 +1,13 @@
+package com.wipro.productviewms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductViewMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductViewMsApplication.class, args);
+	}
+
+}
