77cf1775d8958b212704f4317829df1dc8a323f6
Abhishek Roy <roy.of.abhishek13@gmail.com>
2019-10-20 17:16:20 +0530

    Update Product View

--- src/main/java/com/wipro/productviewms/ProductViewMsApplication.java
+++ src/main/java/com/wipro/productviewms/ProductViewMsApplication.java
@@ -5,14 +5,26 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableCircuitBreaker
+@EnableSwagger2
 public class ProductViewMsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductViewMsApplication.class, args);
+	}
+	@Bean
+	public Docket productViewApi() {
+	    return new Docket(DocumentationType.SWAGGER_2).select()
+	        .apis(RequestHandlerSelectors.basePackage("com.wipro.productviewms")).build();
 	}
 
 }
